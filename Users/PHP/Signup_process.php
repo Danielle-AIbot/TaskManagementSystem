dc123ce11f6ba53ec0288321c4021f7a152a10a4
Danielle-AIbot <dm.vales@mlgcl.edu.ph>
2025-01-04 21:56:56 +0800

    Initial commit

--- Users/PHP/Signup_process.php
+++ Users/PHP/Signup_process.php
@@ -2,8 +2,8 @@
 include 'db.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'];
-    $email = $_POST['email'];
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
     $profile_image = $_FILES['profile_image']['name'];
 
@@ -11,12 +11,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $target_file = $target_dir . basename($_FILES["profile_image"]["name"]);
 
     if (move_uploaded_file($_FILES["profile_image"]["tmp_name"], $target_file)) {
-        // Log the signup action
-        $user_id = mysqli_insert_id($conn);
-        $activity = "Signed up";
         $sql = "INSERT INTO user (username, email, password, profpicture) VALUES ('$username', '$email', '$password', '$profile_image')";
 
         if (mysqli_query($conn, $sql)) {
+            // Log the signup action
+            $user_id = mysqli_insert_id($conn);
+            $activity = "Signed up";
+            $sql_activity = "INSERT INTO activities (user_id, username, activity, created_at) VALUES ($user_id, '$username', '$activity', NOW())";
+            mysqli_query($conn, $sql_activity);
+
+            // Set session variables
+            session_start();
+            $_SESSION['user_id'] = $user_id;
+            $_SESSION['username'] = $username;
+
             header("Location: Dashboard.php");
             exit();
         } else {
